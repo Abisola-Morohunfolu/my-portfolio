3229f09134c04347c98ccb0e616939de89ed9ec0
Bisola <bisola@equally.ai>
2024-07-06 17:41:03 +0100

    added tailwind build files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        background: 'var(--background)',
+        light_background: 'var(--light-background)',
+        secondary: 'var(--color-dark)',
+        accent: 'var(--color-accent)'
+      },
+    },
+  },
+  plugins: [],
+};
