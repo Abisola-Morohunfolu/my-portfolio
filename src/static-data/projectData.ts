681014ec3aab2f6d72e78d4c8efe43ae81e7de05
Bisola <bisola@equally.ai>
2024-07-08 00:01:50 +0100

    updated project image urls

--- src/static-data/projectData.ts
+++ src/static-data/projectData.ts
@@ -5,7 +5,7 @@ export const Projects = [
 		title: 'Flowy Extension',
 		description: 'A basic ecommerce store built with React, powered with fakestoreapi',
 		demoLink: 'https://chromewebstore.google.com/detail/flowy/fjkmnbfpagmpblgkdflondomjkaejipi?pli=1',
-		image: 'https://res.cloudinary.com/abisola/image/upload/v1615895191/tryfit_xmr8y9.png',
+		image: 'https://res.cloudinary.com/abisola/image/upload/v1720393167/Screenshot_2024-07-07_at_23.54.14_rma7z7.png',
 	},
 	{
 		id: 1,
@@ -13,7 +13,7 @@ export const Projects = [
 		title: 'Equally AI Web Accessibility Widget',
 		description: 'A landing page inspired by the wibe website',
 		demoLink: 'https://equally.ai/interface',
-		image: 'https://res.cloudinary.com/abisola/image/upload/v1615895194/vibe_cxueh8.png',
+		image: 'https://res.cloudinary.com/abisola/image/upload/v1720393166/Screenshot_2024-07-07_at_23.53.52_gmiikm.png',
 	},
 	{
 		id: 2,
@@ -23,7 +23,7 @@ export const Projects = [
 			'A web based app that displays the details of countries all over the world built with react and powered by restcountries api',
 		demoLink: 'https://testassessify.com',
 		image:
-			'https://res.cloudinary.com/abisola/image/upload/v1615895197/where_in_the_world_auoyq5.png',
+			'https://res.cloudinary.com/abisola/image/upload/v1720393166/Screenshot_2024-07-07_at_23.54.59_ek3bjo.png',
 	},
 	{
 		id: 4,
