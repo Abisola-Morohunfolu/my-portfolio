43acd47785ba7b7218e14e7eecaed6cd1c70fc83
Bisola <bisola@equally.ai>
2026-01-05 12:54:45 +0100

    audioeye component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
+import AudioEye from "./components/AudioEye";
 
 function App() {
   return (
     <div className="App">
+      <AudioEye />
       <Home />
     </div>
   );
